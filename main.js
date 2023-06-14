25379b0c0caf7cb124f04efac377930d1eed943f
BDeWinne <68663886+BDeWinne@users.noreply.github.com>
2023-06-13 22:22:18 -0300

    balas añadidas

---
+++ main.js
@@ -0,0 +1,36 @@
+import Game from "./public/assets/scenes/game.js";
+import Preload from "./public/assets/scenes/preloader.js";
+
+// Create a new Phaser config object
+const config = {
+  type: Phaser.AUTO,
+  width: 480,
+  height: 240,
+  scale: {
+    mode: Phaser.Scale.FIT,
+    autoCenter: Phaser.Scale.CENTER_BOTH,
+    min: {
+      width: 480,
+      height: 240,
+    },
+    max: {
+      width: 2400,
+      height: 1500,
+    },
+  },
+  physics: {
+    default: "arcade",
+    arcade: {
+      gravity: { y: 1000 },
+      debug: true,
+    },
+  },
+  // List of scenes to load
+  // Only the first scene will be shown
+  // Remember to import the scene before adding it to the list
+  scene: [Preload, Game],
+  zoom: 2,
+};
+
+// Create a new Phaser game instance
+window.game = new Phaser.Game(config);
